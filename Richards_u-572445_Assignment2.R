2141cf1aabf927693314140b06591092c38e3eea
Mikhail Shaw <mikhailshaw@Mikhails-MacBook-Pro.local>
2019-12-10 22:25:56 -0500

    Answers: - 4 to 9 - started 10

--- Richards_u-572445_Assignment2.R
+++ Richards_u-572445_Assignment2.R
@@ -6,6 +6,8 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(caret)
+library(stringr)
+library(e1071)
 
 ## Load data -------------------------------------------------------------------
 #dataDir <- "./input/"
@@ -48,28 +50,148 @@ mushroom_edibility <- get_edibility(mushrooms, edibility)
 answer3_a <- mushroom_edibility
 
 get_allArea_species <- function(mdf, sdf){
-  df <- mdf %>% filter(match(species,sdf$species) && rowSums(survey != 0))
+  df <- mdf %>% filter(match(species,sdf$species) & rowSums(survey != 0))
   return(df)
 }
 
+# & sdf$january != "0"
+# & sdf$february != "0"
+# & sdf$march != "0"
+# & sdf$april != "0"
+# & sdf$may != "0"
+# & sdf$june != "0"
+# & sdf$july != "0"
+# & sdf$august != "0"
+# & sdf$september != "0"
+# & sdf$october != "0"
+# & sdf$november != "0"
+# & sdf$december != "0")
+
 get_allArea_species(mushrooms, survey) %>% glimpse()
+
+bleh = get_allArea_species(mushrooms, survey)
+
+bleh$species[bleh$species == "bulbroot"] %>% glimpse()
 
 df <- mushrooms %>% filter(species %in% survey$species && any(survey == "0"))
 df %>% glimpse()
 
 ## Exercise 4 ------------------------------------------------------------------
+
+reshape_mushroom <- function(mdf){
+  df <- mushrooms %>% gather(mushroom_part, color_of_part, cap_color, 
+  gill_color, spore_print_color, stalk_color, veil_color)
+
+  return(df)
+}
+
+reshape_mushroom(mushrooms) %>% glimpse()
+
+answer4 <- reshape_mushroom(mushrooms)
 ggplot(answer4, aes(x = mushroom_part, fill = color_of_part)) + geom_bar() +
-  scale_fill_manual(values = c("white" = "white", "yellow" = "goldenrod",
-                               "pink" = "lightsalmon2", "buff" = "khaki", "brown" = "brown",
-                               "gray" = "gray", "black" = "black", "green" = "darkolivegreen4",
-                               "purple" = "mediumorchid4", "red" = "firebrick3", "cinnamon" = "tan",
-                               "orange" = "darkorange", "chocolate" = "chocolate"), guide = FALSE)
+scale_fill_manual(values = c("white" = "white", "yellow" = "goldenrod",
+          "pink" = "lightsalmon2", "buff" = "khaki", "brown" = "brown",
+          "gray" = "gray", "black" = "black", "green" = "darkolivegreen4",
+          "purple" = "mediumorchid4", "red" = "firebrick3", "cinnamon" = "tan",
+          "orange" = "darkorange", "chocolate" = "chocolate"), guide = FALSE)
 ## Exercise 5 ------------------------------------------------------------------
+raw_survey <- read.delim("./raw_survey.txt", skip=4, stringsAsFactors = FALSE)
+
+get_new_survey <- function(rdf){
+  df <- rdf %>% rename(
+    species=X, january=X.1, february=X.2, march=X.3, april=X.4, may=X.5,
+    june=X.6, july=X.7, august=X.8, september=X.9, october=X.10, 
+    november=X.11, december=X.12
+  ) %>% mutate_at(vars(-species), as.numeric) %>% 
+  mutate_all(replace_na, replace=0)
+
+  return(df)
+}
+
+# get_new_survey(raw_survey) %>% glimpse()
+
+answer5 <- get_new_survey(raw_survey)
 ## Exercise 6 ------------------------------------------------------------------
+species_model <- train(
+  species ~.,
+  method = "knn",
+  trControl = trainControl(method="cv", number=3),
+  data=mushrooms
+)
+
+species_model
+
+summary(species_model)
+
+answer6 <- species_model
+
 ## Exercise 7 ------------------------------------------------------------------
+
+fit_bruise_model <- function(mdf){
+  df <- mdf %>%
+  mutate(odor = odor, ref="none")
+
+  set.seed(1) 
+  bruises_model <- train(
+  bruises ~ (cap_color + odor),
+  method = "glm",
+  data = df,
+  trControl = trainControl(method = 'cv', number = 3),
+  family = binomial(link = "logit")
+  )
+
+  return(bruises_model)
+
+}
+
+bruise_model <- fit_bruise_model(mushrooms)
+summary(bruise_model)
+
+answer7 <- bruise_model
 ## Exercise 8 ------------------------------------------------------------------
+edibile_survey <- function(sdf, edf){
+  df <- sdf %>% 
+  mutate(edibility = edf$edibility[match(species,edf$species)]) %>%
+  filter(edibility == "edible") %>%
+  gather(month, count, january:december) %>% 
+  mutate(month = paste(toupper(substring(month,1,1)),
+  substring(month, 2,3), sep="")) %>%
+  mutate(month = match(month, month.abb))
+  
+  return(df)
+}
+
+# edibile_survey(survey, edibility) %>% glimpse()
+
+answer6 <- edibile_survey(survey, edibility)
+
+ggplot(answer6, aes(x=month, y=count, 
+group=species, color=species)) + geom_line()
+
 ## Exercise 9 ------------------------------------------------------------------
+ new_mush <- function(mdf){
+   df <- mushrooms %>%
+   mutate_at(vars(contains('cap')), funs(paste(.,".cap", sep=""))) %>%
+   mutate_at(vars(contains('gill')), funs(paste(.,".gills", sep=""))) %>%
+   mutate_at(vars(contains('stalk')), funs(paste(.,".stalk", sep=""))) %>%
+   mutate_all(toupper) %>%
+   rename_all(toupper) %>%
+   rename_all(funs(str_replace(., "_", ".")))
+
+   return(df)
+ }
+
+ new_mush(mushrooms) %>% glimpse()
+
+ answer9 <- new_mush(mushrooms)
 ## Exercise 10 ------------------------------------------------------------------
-edible_shrooms <- sample(edibility$species, 4)
-backup_shrooms <- mushrooms
-backup_shrooms$edibility = "poisonous" backup_shrooms$edibility[backup_shrooms$species %in% edible_shrooms] <- "edible"
+train_test_split <- function(df, P){
+  set.seed(1)
+  trn_index = createDataPartition(y = df$edibility, p = P, list = FALSE)
+  trn_df = df[trn_index, ]
+  tst_df = df[-trn_index, ]
+
+  return(list("trn"=trn_df, "tst"=tst_df))
+}
+
+edible <- train_test_split(answer3_a,0.8)
